ad260e7cc63f2ccf326266f9150f4b45ed02ece1
nadavnavot <125365166+nadavnavot@users.noreply.github.com>
2024-06-07 19:34:35 +0100

    component imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Navbar from "@/components/ui/navbar";
 import Title from "@/components/ui/title";
 import InsightsCarousel from "@/components/ui/insightCarousel";
@@ -6,16 +6,24 @@ import BudgetCarousel from "@/components/ui/budgetCarousel";
 import GoalsCarousel from "@/components/ui/goalsCarousel";
 import NetWorth from "@/components/ui/netWorth";
 import BarChart from "@/components/ui/barChart";
+import PieChart from "@/components/ui/pieChart";
+import BalanceChart from "@/components/ui/balanceChart";
+import CategorizeText from "@/components/ui/categorizeText";
 
 const Overview = () => (
   <div>
-    <Title>Overview</Title>
-    <NetWorth />
-    <InsightsCarousel />
-    <BarChart />
-    <BudgetCarousel />
-    <GoalsCarousel />
-    <div className="mb-48"></div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 overflow-x-hidden">
+      <Title>Overview</Title>
+
+      <NetWorth />
+      <InsightsCarousel />
+      <BalanceChart />
+      <BarChart />
+      <PieChart />
+      <BudgetCarousel />
+      <GoalsCarousel />
+      <CategorizeText />
+    </div>
     <Navbar />
   </div>
 );
