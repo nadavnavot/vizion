d4981b1a13c14b10d6b20a686254d251f4fe59bf
nadavnavot <125365166+nadavnavot@users.noreply.github.com>
2024-06-10 14:01:48 +0100

    timeout debug

--- src/app/db/config/database.js
+++ src/app/db/config/database.js
@@ -9,7 +9,9 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      maxTimeMS: 300000,
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      connectTimeoutMS: 30000,
     });
     console.log("Database connected successfully");
   } catch (error) {
